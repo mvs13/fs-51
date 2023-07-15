b80f72d6fcdce8b847b944708ffe096f35230b2a
Max V. Shumakov <shumakovmv@gmail.com>
2023-07-15 11:23:47 +0300

    fs-51 start commit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -13,7 +13,7 @@ export class AuthService {
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findOne(email);
     if (user && (await bcrypt.compare(pass, user.password))) {
-      const { password, ...result } = user;
+      const { ...result } = user;
       return result;
     }
     return null;
